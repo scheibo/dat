9439b9bcfe9132ce1c029509ab90604ae7fab20d
Kirk Scheibelhut <kjs@scheibo.com>
2011-09-15 15:18:36 -0400

    More spec-tactular

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -4,15 +4,17 @@ describe Dat::Logic do
   include Dat
 
   before(:all) do
-    @d = Dict.new
+    @dict = Dict.new
   end
 
   it "should behave the same for c and pure versions" do
-    Logic.perturb('WALKED', @d).should == Pure::Logic.perturb('WALKED', @d)
+    Logic.perturb('WALKED', @dict).should == Pure::Logic.perturb('WALKED', @dict)
+    Logic.perturb('DAT', @dict).should == Pure::Logic.perturb('DAT', @dict)
   end
 
   it "should behave the same for upper and lower case" do
-    pending
+    Logic.perturb('DAT', @dict).should == Logic.perturb('dat', @dict)
+    Pure::Logic.perturb('DAT', @dict).should == Pure::Logic.perturb('dat', @dict)
   end
 end
 
