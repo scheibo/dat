c2ff83c729b8ec5794e94d240fc48cd2928d2289
Kirk Scheibelhut <kjs@scheibo.com>
2011-09-23 15:32:16 -0400

    Add cdict back

---
+++ ext/cdict/table.h
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 1994,1995,1996,1997 by David R. Hanson.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * <http://www.opensource.org/licenses/mit-license.php>
+ */
+
+#ifndef TABLE_H
+#define TABLE_H
+typedef struct T *Table;
+extern Table table_new(int hint,
+  int cmp(const void *x, const void *y),
+  unsigned long hash(const void *key));
+extern void table_free(Table *table);
+extern int table_length(Table table);
+extern void *table_put(Table table, const void *key,
+  void *value);
+extern void *table_get(Table table, const void *key);
+extern void *table_remove(Table table, const void *key);
+extern void table_map(Table table,
+  void apply(const void *key, void **value, void *cl),
+  void *cl);
+extern void **table_to_array(Table table, void *end);
+#endif
