ab0ed10a1dfc3a81893501c7192194bbceeebd9a
Kirk Scheibelhut <kjs@scheibo.com>
2011-09-23 21:07:06 -0400

    Attempt with cdict

--- lib/dat/dict.rb
+++ lib/dat/dict.rb
@@ -1,10 +1,12 @@
 $:.unshift(File.expand_path('../../dat', __FILE__)) unless $:.include?(File.expand_path('../../dat', __FILE__))
 require 'word'
+require 'cdict'
 
 module Dat
   class Dict
     def initialize(opt={})
       @dict = {} # The internal hash which maps string words to Dat::Word objects
+      @cdict = CDict.new
       file = opt[:file] || File.open(File.expand_path("../../../data/dict", __FILE__))
       bogus = opt[:bogus] || File.open(File.expand_path("../../../data/bogus", __FILE__))
       import file
@@ -12,8 +14,8 @@ module Dat
     end
 
     def [](word)
-      #@dict[word.upcase]
-      @dict[word]
+      #@dict[word]
+      @cdict.include?(word)
     end
 
     def each(&block)
@@ -47,7 +49,14 @@ module Dat
         end
         Word.relatives(*(rels.map {|r| get(r)}), get(word, defn))
       end
+      fill!
       file.close
+    end
+
+    def fill!
+      @dict.each do |k,v|
+        @cdict.add(k)
+      end
     end
 
     def remove(file)
