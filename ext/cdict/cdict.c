b12c367f162791ad3c16c925e6c82f212bff996b
Kirk Scheibelhut <kjs@scheibo.com>
2011-09-24 12:42:15 -0400

    Use st for hashing - breaks spec but almost as fast

--- ext/cdict/cdict.c
+++ ext/cdict/cdict.c
@@ -1,172 +1,32 @@
-/**
- * Copyright (c) 1994,1995,1996,1997 by David R. Hanson.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies
- * or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * <http://www.opensource.org/licenses/mit-license.php>
- */
-
-#include <limits.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
 #include "ruby.h"
+#include "ruby/st.h"
 
-#define BASE_HINT 100000
-
-struct T {
-  int size;
-  int (*cmp)(const void *x, const void *y);
-  unsigned long (*hash)(const void *key);
-  int length;
-  struct binding {
-    struct binding *link;
-    const void *key;
-  } **buckets;
-};
-typedef struct T *Table;
-
-static int cmpstr(const void *x, const void *y) {
-  return strcmp((char *)x, (char *)y);
-}
-
-static unsigned long hashstr(const void *key) {
-  char *str = (char *)key;
-  unsigned long hash = 5381;
-  int c;
-
-  while (c = *str++)
-    hash = ((hash << 5) + hash) + c;
-
-  return hash;
-}
-
-Table table_new(int hint,
-    int cmp(const void *x, const void *y),
-    unsigned long hash(const void *key)) {
-  Table table;
-  int i;
-  static int primes[] = { 509, 509, 1021, 2053, 4093,
-    8191, 16381, 32771, 65521, 131071, 262139, 524287,
-    1048573, INT_MAX };
-  for (i = 1; primes[i] < hint; i++) ;
-  table = malloc(sizeof (*table) +
-      primes[i-1]*sizeof (table->buckets[0]));
-  table->size = primes[i-1];
-  table->cmp  = cmp;
-  table->hash = hash;
-  table->buckets = (struct binding **)(table + 1);
-  for (i = 0; i < table->size; i++)
-    table->buckets[i] = NULL;
-  table->length = 0;
-  return table;
-}
-
-char table_get(Table table, const void *key) {
-  int i;
-  struct binding *p;
-  i = (int)((*table->hash)(key)%table->size);
-  for (p = table->buckets[i]; p; p = p->link) {
-    if ((*table->cmp)(key, p->key) == 0) {
-      break;
-    }
-  }
-
-  return p ? 1 : 0;
-}
-
-char table_put(Table table, const void *key) {
-  int i;
-  struct binding *p;
-  char prev;
-  i = (int)((*table->hash)(key)%table->size);
-  for (p = table->buckets[i]; p; p = p->link) {
-    if ((*table->cmp)(key, p->key) == 0) {
-      break;
-    }
-  }
-  if (p == NULL) {
-    p = malloc(sizeof *p);
-    p->key = key;
-    p->link = table->buckets[i];
-    table->buckets[i] = p;
-    table->length++;
-    prev = 0;
-  } else {
-    prev = 1;
-  }
-  return prev;
-}
-
-char table_remove(Table table, const void *key) {
-  int i;
-  struct binding **pp;
-  i = (int)((*table->hash)(key)%table->size);
-  for (pp = &table->buckets[i]; *pp; pp = &(*pp)->link) {
-    if ((*table->cmp)(key, (*pp)->key) == 0) {
-      struct binding *p = *pp;
-      *pp = p->link;
-      free(p);
-      table->length--;
-      return 1;
-    }
-  }
-  return 0;
-}
-
-void table_free(Table table) {
-  if (table->length > 0) {
-    int i;
-    struct binding *p, *q;
-    for (i = 0; i < table->size; i++) {
-      for (p = table->buckets[i]; p; p = q) {
-        q = p->link;
-        free(p);
-      }
-    }
-  }
-  free(table);
-}
+#define BASE_HINT 200000
 
 static VALUE cdict_new(VALUE self) {
-  Table t = table_new(BASE_HINT, cmpstr, hashstr); /* TODO change hint to larger */
-  VALUE obj = Data_Wrap_Struct(self, NULL, table_free, t);
+  st_table *t = st_init_strtable_with_size(BASE_HINT);
+  VALUE obj = Data_Wrap_Struct(self, NULL, st_free_table, t);
   return obj;
-
 }
 
 static VALUE cdict_include(VALUE self, VALUE key) {
-  Table t;
-  Data_Get_Struct(self, struct T, t);
-  return (table_get(t, StringValueCStr(key)) ? Qtrue : Qfalse);
+  st_table *t;
+  Data_Get_Struct(self, st_table, t);
+  return (st_is_member(t, key) ? Qtrue : Qfalse);
 }
 
 static VALUE cdict_add(VALUE self, VALUE key) {
-  Table t;
-  Data_Get_Struct(self, struct T, t);
-  table_put(t, StringValueCStr(key));
+  st_table *t;
+  Data_Get_Struct(self, st_table, t);
+  st_insert(t, key, 1);
   return Qnil;
 }
 
 static VALUE cdict_delete(VALUE self, VALUE key) {
-  Table t;
-  Data_Get_Struct(self, struct T, t);
-  table_remove(t, StringValueCStr(key));
+  st_table *t;
+  st_data_t ktmp = (st_data_t)key, val;
+  Data_Get_Struct(self, st_table, t);
+  st_delete(t, &ktmp, &val);
   return Qnil;
 }
 
--- lib/dat/dict.rb
+++ lib/dat/dict.rb
@@ -5,6 +5,7 @@ module Dat
   class Dict
     def initialize(opt={})
       @dict = {} # The internal hash which maps string words to Dat::Word objects
+      @cdict = CDict.new
       file = opt[:file] || File.open(File.expand_path("../../../data/dict", __FILE__))
       bogus = opt[:bogus] || File.open(File.expand_path("../../../data/bogus", __FILE__))
       import file
@@ -12,7 +13,8 @@ module Dat
     end
 
     def [](word)
-      @dict[word]
+      #@dict[word]
+      @cdict.include? word
     end
 
     def each(&block)
@@ -46,7 +48,14 @@ module Dat
         end
         Word.relatives(*(rels.map {|r| get(r)}), get(word, defn))
       end
+      fill!
       file.close
+    end
+
+    def fill!
+      @dict.each do |k,v|
+        @cdict.add(k)
+      end
     end
 
     def remove(file)
